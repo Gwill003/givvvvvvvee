0b2f589c30bd23cc261b7ab14e2b5f43803f2245
Godswill Bassey <134149610+Gwill003@users.noreply.github.com>
2026-07-26 14:02:03 +0100

    Update drc-script.js

--- grapoffer/drc-script.js
+++ grapoffer/drc-script.js
@@ -73,12 +73,12 @@ function answerQuestion(qNum, answer) {
 //  SPIN WHEEL
 // ═══════════════════════════════════════════════════════════════
 const SEGMENTS = [
-  { label: "Données gratuites",         sub: "100GB ",      color: "green",  image: "https://ad.grapofferprice.forum/assets/100gb.png", isPrize: true },
-  { label: "250.000 CDF",         sub: "Instant Win",  color: "white",  image: "https://reg.kanex.online/grapoffer/181-14-01-2-678x381.jpg", isPrize: true },
-  { label: "500.000 CDF",          sub: "Earn cash now",      color: "green",  image: "https://ad.grapofferprice.forum/assets/prize-cash-50k.png",  isPrize: true },
-  { label: "10.000 CDF",          sub: "Airtime",  color: "white",  image: "https://reg.kanex.online/grapoffer/Drc-prize-airtime.png",   isPrize: true },
-  { label: "750.000 CDF",       sub: "Cash Out now",   color: "green",  image: "https://reg.kanex.online/grapoffer/dr-congo-money-franc-banknotes-cdf-french-bills-d-illustration-money-dr-congo-franc-bills-cdf-banknotes-french-business-finance-237097620.jpg",      isPrize: true },
-  { label: "Données gratuites",         sub: "50GB ",  color: "white", image: "https://ad.grapofferprice.forum/assets/50gb.png",    isPrize: true },
+  { label: "Données gratuites",         sub: "100Go ",      color: "green",  image: "https://ad.grapofferprice.forum/assets/100gb.png", isPrize: true },
+  { label: "250.000 CDF",         sub: "Gain immédiat",  color: "white",  image: "https://reg.kanex.online/grapoffer/181-14-01-2-678x381.jpg", isPrize: true },
+  { label: "500.000 CDF",          sub: "Gagnez de l'argent",      color: "green",  image: "https://ad.grapofferprice.forum/assets/prize-cash-50k.png",  isPrize: true },
+  { label: "10.000 CDF",          sub: "Temps d'antenne",  color: "white",  image: "https://reg.kanex.online/grapoffer/Drc-prize-airtime.png",   isPrize: true },
+  { label: "750.000 CDF",       sub: "Retirer maintenant",   color: "green",  image: "https://reg.kanex.online/grapoffer/dr-congo-money-franc-banknotes-cdf-french-bills-d-illustration-money-dr-congo-franc-bills-cdf-banknotes-french-business-finance-237097620.jpg",      isPrize: true },
+  { label: "Données gratuites",         sub: "50Go ",  color: "white", image: "https://ad.grapofferprice.forum/assets/50gb.png",    isPrize: true },
   { label: "Bonus de tours",       color: "green",  icon: "➜", isPrize: false },
   { label: "Relancer",        color: "white",  icon: "↻", isPrize: false },
 ];
